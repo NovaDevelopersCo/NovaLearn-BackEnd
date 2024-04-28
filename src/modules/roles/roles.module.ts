acdc5d48cf1834fdb293207d55574eea5e362fc2
Zoom <egor147536987@gmail.com>
2024-04-28 13:46:07 +0300

    Add role-based authorization to RolesController
    The changes include:
    
    * Importing `UseGuards`, `Roles`, and `RolesGuard` from `@nestjs/common`, `@nestjs

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { RolesService } from './roles.service'
 import { RolesController } from './roles.controller'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { Role } from './model/roles.model'
 import { User } from '../users/model/users.model'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
     providers: [RolesService],
     controllers: [RolesController],
-    imports: [SequelizeModule.forFeature([Role, User])],
+    imports: [
+        SequelizeModule.forFeature([Role, User]),
+        forwardRef(() => AuthModule),
+    ],
     exports: [RolesService],
 })
 export class RolesModule {}
