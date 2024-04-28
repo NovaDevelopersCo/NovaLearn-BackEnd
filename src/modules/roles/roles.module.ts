ae59fea620e31bbcce00838912f77d68329021b7
Zoom <egor147536987@gmail.com>
2024-04-28 13:43:17 +0300

    Remove many-to-many relationship between User and Role; Replace it with one-to-many in User model
    The diff shows changes in the models for User and Role, as well as the removal of the UserRoles model.

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -4,12 +4,11 @@ import { RolesController } from './roles.controller'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { Role } from './model/roles.model'
 import { User } from '../users/model/users.model'
-import { UserRoles } from './model/user-roles.model'
 
 @Module({
     providers: [RolesService],
     controllers: [RolesController],
-    imports: [SequelizeModule.forFeature([Role, User, UserRoles])],
+    imports: [SequelizeModule.forFeature([Role, User])],
     exports: [RolesService],
 })
 export class RolesModule {}
